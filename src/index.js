d9cd42a0b84da690571d20321c5730517edacd6c
«AlexeyGoroshchenya» <375295509897@yandex.ru>
2022-04-22 10:46:36 +0300

    подключил бургер

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { swiper } from './modules/swiper';
+import { scroll } from './modules/scroll';
+import { burger } from './modules/burger';
+import { sendForm } from './modules/sendForm';
+
+
+//swiper();
+//scroll();
+burger();
+//sendForm();
